a9805ae69783862431e7e1abf42d3fa7fa1ec65e
Naseem Sarwar <naseem@iiadigital.co.uk>
2023-06-23 16:07:38 +0100

    Refactor invoice factory name

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
  */
-class InvoicesFactory extends Factory
+class InvoiceFactory extends Factory
 {
     /**
      * Define the model's default state.
